2469f4415fd3c3f4a4e8b2d50264de49e6fe5968
Kayo <kayo.fernandes@@allrede.net.br>
2022-10-27 09:49:03 -0300

    update

--- banco.py
+++ banco.py
@@ -1,44 +1,31 @@
-import sqlite3
 from datetime import datetime
 
+from peewee import *
 
-class BancoDados():
-    def __init__(self) -> None:
-        self.con = sqlite3.connect("pydb.db")
-        self.create_table()
+db = SqliteDatabase('pydb.db')
 
-    def create_table(self):
-        cursor = self.con.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS users (id_users INTEGER PRIMARY KEY, email TEXT, passwrd TEXT, UNIQUE(email) )")
-        self.con.commit()
-        cursor.execute("CREATE TABLE IF NOT EXISTS last_login (id_last_login INTEGER PRIMARY KEY, id_users INTEGER, last_login TEXT,remember INTEGER, FOREIGN KEY (id_users) REFERENCES users(id_users) )")
-        self.con.commit()
-            
-    def insert_table_users(self,values:list):
-        cursor = self.con.cursor()
-        cursor.execute(f"INSERT INTO users VALUES (NULL,'{values[0]}','{values[1]}')")
-        self.con.commit()
-        cursor.close()
+def create_table():
+    with db:
+        db.create_tables([Users,LastLogin])
+        print('Tabelas criada')
 
-    def select_table_users(self):
-        cursor = self.con.cursor()
-        resultado = cursor.execute('SELECT * FROM users')
-        cursor.close()
-        return resultado
-    
-    def select_email_password_users(self,email:str,password:str):
-        cursor = self.con.cursor()
-        resultado = cursor.execute(f"SELECT * FROM users WHERE email = '{email}' and passwrd = '{password}'")
-        return resultado.fetchone()
-    
-    def insert_table_last_login(self,id_users:int,remember:int):
-        cursor = self.con.cursor()
-        cursor.execute(f"INSERT INTO last_login VALUES (NULL,{id_users},'{datetime.today().strftime('%Y-%m-%d %H:%M:%S')}',{1 if remember else 0})")
-        self.con.commit()
-        cursor.close()
-    
-    def select_table_last_login(self,id_users:int):
-        cursor = self.con.cursor()
-        resultado = cursor.execute(f"SELECT remember FROM last_login WHERE id_users={id_users}")
-        cursor.close()
-        return resultado.lastrowid
+class BaseModel(Model):
+
+    class Meta:
+        database = db
+        
+
+
+class Users(BaseModel):
+    name = CharField()
+    email = CharField(unique=True)
+    password = CharField()
+
+
+class LastLogin(BaseModel):
+    user = ForeignKeyField(Users)
+    date = DateTimeField(default=datetime.now())
+    remember = BooleanField()
+
+if __name__ == '__main__':
+    create_table()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import gi
 
-from banco import BancoDados
+from banco import Users
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -9,60 +9,73 @@ from gi.repository import Gtk
 class Manipulador(object):
     def __init__(self) -> None:
         self.stack: Gtk.Stack = Builder.get_object('stack')
-        self.banco = BancoDados()
+        self.users = Users
 
     # Destroi oas janelas quando clica em fechar
-    def on_main_window_destroy(self,Window):
+    def on_main_window_destroy(self, Window):
         Gtk.main_quit()
 
     # Clicar em login
-    def on_btn_login_clicked(self,button):
+    def on_btn_login_clicked(self, button):
         email = self.__get_input('input_login_email')
         password = self.__get_input('input_login_password')
-        remember = Builder.get_object('check_lembrar').get_active()
-        self.__login(email,password,remember)
-        
-    def on_btn_cadastrar_clicked(self,button):
+        self.__login(email, password)
+
+    def on_btn_cadastrar_clicked(self, button):
         self.stack.set_visible_child_name('view_cadastro')
+
+    def on_btn_esq_clicked(self, button):
+        self.aviso = Builder.get_object('link_esq_aviso')
+        self.aviso.set_text('')
+        self.stack.set_visible_child_name('view_esqueci')
+
+    def on_btn_esq_cancelar_clicked(self, button):
+        self.aviso.set_text('')
+        self.stack.set_visible_child_name('view_login')
+
+    def on_btn_esq_enviar_clicked(self, button):
+        consulta = self.users.filter(email=self.__get_input('btn_esq_email'))
+        if consulta:
+            self.aviso.set_text('Enviado com sucesso')
+        else:
+            self.aviso.set_text('Email não cadastrado')
+
         
-    def on_btn_cad_salvar_clicked(self,button):
+    def on_btn_cad_salvar_clicked(self, button):
+        name = self.__get_input('input_cad_name')
         email = self.__get_input('input_cad_email')
         password1 = self.__get_input('input_cad_password1')
         password2 = self.__get_input('input_cad_password2')
-        self.__check_validadion(email,password1,password2)
-        
-        
-    def on_btn_cad_cancelar_clicked(self,button):
+        self.__check_validadion(name,email, password1, password2)
+
+    def on_btn_cad_cancelar_clicked(self, button):
         self.stack.set_visible_child_name('view_login')
-        
-    
-    def __check_validadion(self,email:str,password1:str,password2:str) -> bool:
+
+    def __check_validadion(self, name:str,email: str, password1: str, password2: str) -> bool:
         if email and password1 == password2:
-            self.__insert_banco(email,password1)
-            self.__msg_login('Cadastro realizado',f'O email *{email}* foi cadastrado com sucesso.','emblem-ok-symbolic')
+            self.users.create(name=name,email=email,password=password1)
+            self.__msg_login(
+                'Cadastro realizado', f'O email *{email}* foi cadastrado com sucesso.', 'dialog-ok')
             return True
         else:
-            self.__msg_login('Aviso',f'Erro ao tentar cadastrar o email *{email}*.','dialog-error')
+            self.__msg_login(
+                'Aviso', f'Erro ao tentar cadastrar o email *{email}*.', 'dialog-error')
             return False
-            
-    def __login(self,email:str,password:str,remember:bool):
-        logando = self.__select_banco(email,password)
+
+    def __login(self, email: str, password: str):
+        logando = self.users.filter(email=email,password=password)
         if logando:
-            self.banco.insert_table_last_login(logando[0],remember)
-            self.__msg_login('Bem-Vindo','Usuario logado com sucesso','emblem-ok-symbolic')
+            
+            self.__msg_login(
+                'Bem-Vindo', 'Usuario logado com sucesso', 'emblem-ok-symbolic')
         else:
-            self.__msg_login('Aviso','Email ou senha incorreto','dialog-error')
-            
-    def __select_banco(self,email:str,password:str):
-        return self.banco.select_email_password_users(email,password)
-    
-    def __insert_banco(self,email:str,password:str):
-        self.banco.insert_table_users([email,password])
-    
-    def __get_input(self,id:str) -> str : 
+            self.__msg_login(
+                'Aviso', 'Email ou senha incorreto', 'dialog-error')
+
+    def __get_input(self, id: str) -> str:
         return Builder.get_object(id).get_text()
-            
-    def __msg_login(self,title:str,body:str,icon:str) -> None:
+
+    def __msg_login(self, title: str, body: str, icon: str) -> None:
         mensagem = Builder.get_object('msg_login')
         mensagem.props.text = title
         mensagem.props.secondary_text = body
@@ -70,6 +83,7 @@ class Manipulador(object):
         mensagem.show_all()
         mensagem.run()
         mensagem.hide()
+
 
 Builder = Gtk.Builder()
 Builder.add_from_file('ui.glade')
